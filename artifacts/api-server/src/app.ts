49df5fcb6af14563ffeddc83b59478ed430e963c
Ryan Martins <ryanhenri085@gmail.com>
2026-09-03 13:33:30 -0300

    feat: publish artifacts/api-server/src/app.ts

---
+++ artifacts/api-server/src/app.ts
@@ -0,0 +1,38 @@
+import express, { type Express } from "express";
+import cors from "cors";
+import pinoHttp from "pino-http";
+import router from "./routes";
+import { logger } from "./lib/logger";
+import { notFoundHandler } from "./middlewares/not-found";
+import { errorHandler } from "./middlewares/error-handler";
+
+const app: Express = express();
+
+app.use(
+  pinoHttp({
+    logger,
+    serializers: {
+      req(req) {
+        return {
+          id: req.id,
+          method: req.method,
+          url: req.url?.split("?")[0],
+        };
+      },
+      res(res) {
+        return {
+          statusCode: res.statusCode,
+        };
+      },
+    },
+  }),
+);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+export default app;
